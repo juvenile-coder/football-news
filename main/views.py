bf27930a16859ac27b727799c6d75ae6439a3c3e
juvenile-coder <jefftirza@gmail.com>
2025-09-27 12:28:19 +0700

    Perbaikan Tutorial 4

--- main/views.py
+++ main/views.py
@@ -11,6 +11,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
 
+
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
